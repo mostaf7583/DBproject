0f55a03b7aa6297862b419b4e12573114c9e0e81
mostafa <mostaf7583@gmail.com>
2021-12-28 03:57:25 +0200

    register complete

---
+++ M3/GUCera/GUCera/register.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="register.aspx.cs" Inherits="GUCera.register" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            PLEASE REGISTER FIRST</div>
+</body>
+    Email<br />
+    <asp:TextBox ID="Email" runat="server"></asp:TextBox>
+    <br />
+    Name<br />
+    <asp:TextBox ID="Name" runat="server"></asp:TextBox>
+    <br />
+    Password :<br />
+    <asp:TextBox ID="password" runat="server"></asp:TextBox>
+    <br />
+    <br />
+    Enter your position :<br />
+    <asp:RadioButton ID="GUCian" runat="server" OnCheckedChanged="RadioButton1_CheckedChanged" Text="GUCian" />
+    <br />
+    <asp:RadioButton ID="NonGUCian" runat="server" Text="NonGUCian" />
+    <br />
+    <asp:RadioButton ID="Supervisor" runat="server" OnCheckedChanged="Supervisor_CheckedChanged" Text="Supervisor" />
+    <br />
+    <asp:RadioButton ID="Examiner" runat="server" Text="Examiner" />
+    <br />
+    <br />
+    <asp:Button ID="Button1" runat="server" onclick="Register" Text="register" style="height: 26px" />
+    <br />
+    </form>
+
+</html>
---
+++ M3/GUCera/GUCera/register.aspx.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GUCera
+{
+    public partial class register : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Register(object sender, EventArgs e)
+        {
+            String connStr = WebConfigurationManager.ConnectionStrings["GUCera"].ToString();       
+            SqlConnection conn = new SqlConnection(connStr);
+
+            String pass = password.Text;
+            String email = Email.Text; 
+            String name = Name.Text;
+            if (GUCian.Checked == true )
+            {
+                SqlCommand registerGUCproc = new SqlCommand("StudentRegister2",conn);
+                StudentRegister2.Parameters.Add(new SqlParameter("@password",pass));
+                StudentRegister2.Parameters.Add(new SqlParameter("@GUCian", 1));
+                StudentRegister2.Parameters.Add(new SqlParameter("@email", Email));
+
+            }else
+            if (NonGUCian.Checked == true)
+            {
+                SqlCommand registerNOnGUCianproc = new SqlCommand("StudentRegister2", conn);
+                StudentRegister2.Parameters.Add(new SqlParameter("@password", pass));
+                StudentRegister2.Parameters.Add(new SqlParameter("@GUCian", 0));
+                StudentRegister2.Parameters.Add(new SqlParameter("@email", Email));
+
+            }
+            else
+            if(Examiner.Checked == true)
+            {
+                SqlCommand registerExaminerproc = new SqlCommand("examinerRegister", conn);
+                examinerRegister.Parameters.Add(new SqlParameter("@password", pass));
+                examinerRegister.Parameters.Add(new SqlParameter("@email", Email));
+
+            }
+            else
+            if (Supervisor.Checked == true)
+            {
+                SqlCommand registerSupervisorproc = new SqlCommand("SupervisorRegister2", conn);
+                SupervisorRegister2.Parameters.Add(new SqlParameter("@password", pass));
+                SupervisorRegister2.Parameters.Add(new SqlParameter("@email", Email));
+
+            }
+
+
+            SqlCommand registerproc = new SqlCommand("StudentRegister2",conn);
+        }
+
+        protected void RadioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Supervisor_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
